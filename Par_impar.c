ba75613adc77610e5dee6def2c24c4880e158ec7
JJ <jjhdzh@gmail.com>
2016-09-16 21:32:09 -0500

    Create Par_impar.c
    Par o ipar C

---
+++ Par_impar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int n;
+printf("Ingrese un valor: \n");
+scanf("%d",&n);
+
+if (n%2 ==0){
+	printf("%d es par \n",n);
+}
+else{
+	printf("%d es impar \n",n);
+}
+	return 0;
+}
